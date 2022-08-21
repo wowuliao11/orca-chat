960e10a040b0da1c599dad29d2f6757bfeb91562
baozhiming <wowuliao@outlook.com>
2022-08-21 22:42:06 +0800

    first websocket

---
+++ src/boot/socketio.ts
@@ -0,0 +1,17 @@
+import { boot } from 'quasar/wrappers';
+import { io } from 'socket.io-client';
+
+const socket = io('http://localhost:4444');
+
+export default boot(async (/* { app, router, ... } */) => {
+  // client-side
+  socket.on('connect', () => {
+    console.log(socket.id); // x8WIv7-mJelg7on_ALbx
+  });
+
+  socket.on('disconnect', () => {
+    console.log(socket.id); // undefined
+  });
+});
+
+export { socket };
---
+++ src/pages/room/RoomPage.vue
@@ -0,0 +1,60 @@
+<template>
+  <q-card class="my-card" flat bordered>
+    <q-item>
+      <q-item-section avatar>
+        <q-avatar>
+          <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+        </q-avatar>
+      </q-item-section>
+
+      <q-item-section>
+        <q-item-label>ROOM TITLE</q-item-label>
+        <q-item-label caption> haha </q-item-label>
+      </q-item-section>
+    </q-item>
+
+    <q-separator />
+
+    <q-card-section>
+      <!-- chat content -->
+      <q-chat-message
+        v-for="(r, index) in river"
+        :key="index"
+        :name="r.user?.name || 'unknowName'"
+        :avatar="`https://cdn.quasar.dev/img/avatar${(index % 2) + 1}.jpg`"
+        :text="[r.msg]"
+        :sent="r.send"
+      />
+    </q-card-section>
+
+    <q-card-section>
+      <q-input
+        v-model="message"
+        label="I WANT TO SAY!!"
+        @keydown.enter.prevent="onSend"
+      />
+    </q-card-section>
+  </q-card>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import { socket } from 'boot/socketio';
+
+const river = ref<Array<{ msg: string; user: any; send: boolean }>>([]);
+
+const message = ref('');
+
+const onSend = async () => {
+  console.log('go');
+  const result = await socket.emit('msgToServer', message.value);
+  console.log(result);
+};
+
+onMounted(async () => {
+  socket.on('msgToClient', async (msg: string) => {
+    console.log('i got:' + msg);
+    river.value.push({ send: false, msg, user: {} });
+  });
+});
+</script>
